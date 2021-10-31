ed6ec7132e277295daf9b38ecf430110f44e8fd2
superdownload124 <80016011+superdownload124@users.noreply.github.com>
2021-10-31 15:27:08 -0500

    Add files via upload

---
+++ CmdScript.bat
@@ -0,0 +1,53 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD5YRQ6DAES0A5EO4f7+086IoVgQUewra7P417C9I/Ia+lHYVpsp5XlIkdsYMDhXcx+qYAt0mXxNu3SRec6fvG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlTMbCXrZg==
+::ZQ05rAF9IAHYFVzEqQITJR9nTxaLLH/a
+::eg0/rx1wNQPfEVWB+kM9LVsJDD2NKV+vBLA45//14+WGpkgPGuA+bO8=
+::fBEirQZwNQPfEVWB+kM9LVsJDCePOFi5FLwM/Ig=
+::cRolqwZ3JBvQF1fEqQITJR9nTxaLLH/a
+::dhA7uBVwLU+EWHqL+U8xJh9sfGQ=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmn+UYDKwldXBDi
+::Zh4grVQjdCyDJGyX8VAjFD5YRQ6DAES0A5EO4f7+086IoVgQUewra7P417C9I/Ia+lHYVpsp5XlIkdsYMDhVeiGofAYkvSBHrmHl
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title CmdScript
+%1
+echo CmdScript
+SET input=
+goto *Count
+goto *Code
+:*Code
+SET var=%cd%
+set /p input= %var% 
+%input%
+SET input=
+goto *Count
+:*Count
+set /a guessnum=%guessnum% +1
+goto *Code
+:Crash
+exit
+::YOUTUBE-DOWNLOADER
+
